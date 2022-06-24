c69064a571cb7cba565f4f3e6d9cb23b42cea518
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-06-24 23:10:44 +0700

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,19 @@
+Info Port:
+   ----- XRAY CORE ----------
+   - XRAY VLESS XTLS SPLICE  : 443
+   - XRAY VLESS XTLS DIRECT  : 443
+   - XRAY VLESS WS TLS       : 443
+   - XRAY TROJAN TCP         : 443
+   - XRAY VMESS TLS          : 443
+   - XRAY VLESS GRPC         : 6643
+   - XRAY VLESS H2           : 100
+   - XRAY VLESS MKCP         : 7443
+   - XRAY VLESS MKCP TLS     : 743
+   - XRAY TROJAN GRPC TLS    : 653
+   - XRAY TROJAN XTLS/TLS    : 6443
+   - XRAY VMESS HTTP TLS     : 643
+   - XRAY VMESS HTTP         : 80
+   - XRAY VMESS TCP TLS      : 535
+   - XRAY VLESS WS NONE      : 88
+   - XRAY VMESS WS NONE      : 888
+   - XRAY Shadowsocks AEAD   : 1111
