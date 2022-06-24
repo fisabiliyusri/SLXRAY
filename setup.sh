f9d94d755bd59441201b85c68f8df7b9a2b2413a
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-06-24 17:50:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,9 +33,15 @@ exit 0
 fi
 mkdir /var/lib/crot;
 echo "IP=" >> /var/lib/crot/ipvps.conf
+#cf
+wget https://raw.githubusercontent.com/fisabiliyusri/SLXRAY/master/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+#install ssh ovpn
+wget https://raw.githubusercontent.com/fisabiliyusri/SLXRAY/master/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 #install v2ray
-wget https://github.com/fisabiliyusri/SLXRAY/xray/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
+wget https://raw.githubusercontent.com/fisabiliyusri/SLXRAY/master/xray/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
 rm -f /root/ins-xray.sh
+rm -f /root/cf.sh
+rm -f /root/ssh-vpn.sh
 systemctl daemon-reload
 history -c
 echo " "
